216a3242b205defdb481d438395e6e93c4b89fc4
zhoufs1 <1325069713@qq.com>
2018-10-11 15:52:58 +0800

    utf-8编码

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
 import _ from 'lodash';
-import './loader-static/style.css';
-import logo from './loader-static/logo.png';
-import data from './loader-static/data.xml';
-
+// import './loader-static/style.css';
+// import logo from './loader-static/logo.png';
+// import data from './loader-static/data.xml';
+import printMe from './output-static/print.js';
 function component() {
     const element = document.createElement('div');
 
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add('hello');
-    const myLogo = new Image();
-    myLogo.src = logo;
-    element.appendChild(myLogo);
-    console.log(data);
+    // element.classList.add('hello');
+    // const myLogo = new Image();
+    // myLogo.src = logo;
+    // element.appendChild(myLogo);
+    // console.log(data);
+
+    const btn = document.createElement('button');
+    btn.innerHTML = 'click me and chick the console！';
+    btn.onclick = printMe;
+    element.appendChild(btn);
     return element;
 }
 
