433fde1373e8adf838892cda3d65e8989091101f
zhoufs1 <1325069713@qq.com>
2018-10-15 11:02:10 +0800

    使用express + dev-server-middleware中间件启动服务

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,8 @@ module.exports = {
     output: {
         // filename: 'bundle.js',
         filename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'dist')
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: '/'
     },
     plugins: [
         new HtmlWebpackPlugin({
